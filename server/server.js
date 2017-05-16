02582548b623224d49a2f172e504c7d34dfdd748
allenBatac <alrenlen@gmail.com>
2017-05-16 10:12:55 +0800

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+app.listen(port, ()=>{
+	console.log('Server is up on port ' + port);
+});
