88ac44c71a427e29a2eb5b3f600373fa4e770161
AWSYS-I\allen.batac <allen.batac>
2017-05-16 10:53:53 +0800

    Adding socket.io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 var path = require('path');
 var express = require('express');
+var socketIO = require('socket.io');
+var http = require('http');
+
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
-app.listen(port, ()=>{
+io.on('connection', (socket) => {
+	console.log('new user connected');
+
+	socket.on('disconnect', () => {
+		console.log('Client disconencted from server');
+	});
+});
+
+server.listen(port, ()=>{
 	console.log('Server is up on port ' + port);
 });
